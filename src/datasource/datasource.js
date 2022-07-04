155708b4e9f5bb1d5b63b5005e362c72ccb6e402
Marcelo <cello.ma16@gmail.com>
2022-07-04 20:27:55 -0300

    some

--- src/datasource/datasource.js
+++ src/datasource/datasource.js
@@ -1,16 +1,13 @@
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from "dotenv";
 
-dotenv.config({
-    path: "../.env"
-});
 
 const objectId = ObjectId;
 let db;
 
 
 async function connectDB() {
-    const mongoClient = new MongoClient(process.env.MONGO);
+    const mongoClient = new MongoClient(process.env.MONGO_URI);
     await mongoClient.connect();
     db = mongoClient.db("wallet");
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import authRouter from "./routes/authRouter.js";
 import transactionRouter from "./routes/transactionRouter.js";
 import { connectDB } from "./datasource/datasource.js";
 
+dotenv.config({
+    path: "../.env"
+});
+
 const app = express();
 
 app.use(cors());
@@ -13,7 +17,7 @@ app.use(authRouter);
 app.use(transactionRouter);
 
 connectDB().then(_ => {
-    app.listen(5000, () => {
+    app.listen(process.env.PORT, () => {
         console.log("Servidor rodando na porta 5000!");
     });
 }).catch(err => console.error("Não foi possível conectar ao servidor!\n\n" + err));
